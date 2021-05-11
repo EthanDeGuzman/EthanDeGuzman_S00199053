912c47520ce4face1b6dfa275731b08580543198
Ethan De Guzman <S00199053@mail.itsligo.ie>
2021-05-11 09:56:21 +0100

    Created database using CodeFirst Entity Framework

--- Game.cs
+++ Game.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace EthanDeGuzman_S00199053
 {
     public class Game
     {
+        public int ID { get; set; }
         public string Name { get; set; }
-        public double Score { get; set; }
+        public double CriticScore { get; set; }
         public string Description { get; set; }
         public string Platform { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string Game_Image { get; set; }
 
         public void DecreasePrice(double number)
         {
-            Price -= number;
+            Price -= (decimal) number;
         }
     }
 
-    
+    public class GameData : DbContext
+    {
+        public GameData() : base("MyGameData") { }
+
+        public DbSet<Game> Game { get; set; }
+    }
 }
