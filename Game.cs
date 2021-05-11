bec3de54ae774de8b45a0c821fddd7a64ceccaf8
Ethan De Guzman <S00199053@mail.itsligo.ie>
2021-05-11 09:36:05 +0100

    Created Game class and added properties

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EthanDeGuzman_S00199053
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public double Price { get; set; }
+        public string Game_Image { get; set; }
+    }
+}
