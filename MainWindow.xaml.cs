6cbdee421cf5a539a34483963c98ee141249e87e
Ethan De Guzman <S00199053@mail.itsligo.ie>
2021-05-11 10:12:21 +0100

    Created XAML interface and show list of games

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace EthanDeGuzman_S00199053
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Game> AllGames;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GameData db = new GameData();
+
+            var query = from g in db.Game
+                        select g;
+
+            AllGames = query.ToList();
+
+            lstGame.ItemsSource = AllGames;
+        }
+
+        private void lstGame_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Game selectedGame = lstGame.SelectedItem as Game;
+
+            if (selectedGame != null)
+            {
+                tblkDetails.Text = string.Format(
+                $"Game Name: {selectedGame.Name}" + $"\nDescription: {selectedGame.Description}"
+                + $"\nPlatform: {selectedGame.Platform}" + $"\nPrice: {selectedGame.Price}"
+                );
+
+            }
         }
     }
 }
